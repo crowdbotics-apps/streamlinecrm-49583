a77fea2b053f0ab4dc0e8303f6ee34f96e68a8a2
Crowdbotics <team@crowdbotics.com>
2024-10-04 16:19:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "streamlinecrm-49583",
+  url: "https://streamlinecrm-49583.botics.co",
+  api: "https://streamlinecrm-49583.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
